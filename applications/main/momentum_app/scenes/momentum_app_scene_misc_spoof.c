dc3378ab540548cdecc9e3433336198825213bd5
Cheng YiHONG <2120490235@zufedfc.edu.cn>
2024-09-22 14:19:07 +0800

    ✨ feat: 增加正版与复刻版切换开关

--- applications/main/momentum_app/scenes/momentum_app_scene_misc_spoof.c
+++ applications/main/momentum_app/scenes/momentum_app_scene_misc_spoof.c
@@ -20,6 +20,16 @@ static void momentum_app_scene_misc_spoof_shell_color_changed(VariableItem* item
     app->require_reboot = true;
 }
 
+// 设备状态变更回调
+static void momentum_app_scene_misc_device_status_changed(VariableItem* item) {
+    MomentumApp* app = variable_item_get_context(item);
+    bool value = variable_item_get_current_value_index(item);
+    variable_item_set_current_value_text(item, value ? "复刻版" : "正版");
+    momentum_settings.spoof_status = value;
+    app->save_settings = true;
+    app->require_reboot = true;
+}
+
 void momentum_app_scene_misc_spoof_var_item_list_callback(void* context, uint32_t index) {
     MomentumApp* app = context;
     view_dispatcher_send_custom_event(app->view_dispatcher, index);
@@ -30,7 +40,7 @@ void momentum_app_scene_misc_spoof_on_enter(void* context) {
     VariableItemList* var_item_list = app->var_item_list;
     VariableItem* item;
 
-    item = variable_item_list_add(var_item_list, "翻盖名称", 0, NULL, app);
+    item = variable_item_list_add(var_item_list, "Flipper 名称", 0, NULL, app);
     variable_item_set_current_value_text(item, app->device_name);
 
     item = variable_item_list_add(
@@ -41,6 +51,15 @@ void momentum_app_scene_misc_spoof_on_enter(void* context) {
         app);
     variable_item_set_current_value_index(item, momentum_settings.spoof_color);
     variable_item_set_current_value_text(item, shell_color_names[momentum_settings.spoof_color]);
+
+    item = variable_item_list_add(
+        var_item_list,
+        "设备状态",
+        2,
+        momentum_app_scene_misc_device_status_changed,
+        app);
+    variable_item_set_current_value_index(item, momentum_settings.spoof_status);
+    variable_item_set_current_value_text(item, momentum_settings.spoof_status ? "复刻版" : "正版");
 
     variable_item_list_set_enter_callback(
         var_item_list, momentum_app_scene_misc_spoof_var_item_list_callback, app);
